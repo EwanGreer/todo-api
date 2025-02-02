506d96ba44b10a9fa94183ef63dd51b8f6a28647
Ewan Greer <58301904+EwanGreer@users.noreply.github.com>
2025-02-02 15:01:05 +0000

    feat: improved logging (#1)

--- main.go
+++ main.go
@@ -1,25 +1,54 @@
 package main
 
 import (
-	"log"
+	"embed"
+	"log/slog"
 	"os"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+const pagesDir = "public/html"
+
+//go:embed public
+var publicFS embed.FS
+
 func main() {
+	l := slog.New(slog.NewJSONHandler(os.Stderr, nil)).With("service", "todo-api")
+	slog.SetDefault(l)
+
 	e := echo.New()
 	e.HideBanner = true
 
-	e.Use(middleware.Logger())
+	e.Use(
+		middleware.RequestID(),
+		middleware.Logger(),
+		middleware.Recover(),
+	)
 
 	e.GET("/", func(c echo.Context) error {
-		return c.JSON(200, map[string]any{"msg": "hello world"})
+		b, err := publicFS.ReadFile(pagesDir + "/index.html")
+		if err != nil {
+			slog.Error("ReadFile", "error", err)
+			return c.HTML(500, "<p>Something went wrong</p>")
+		}
+
+		return c.HTMLBlob(200, b)
+	})
+
+	// TODO: auth (JWT)
+	e.GET("/api/user/:id", func(c echo.Context) error {
+		return c.JSON(200, map[string]any{
+			"msg": map[string]any{
+				"username": "user1",
+				"email":    "dummy@emails.com",
+			},
+		})
 	})
 
 	if err := e.Start("0.0.0.0:8000"); err != nil {
-		log.Print(err)
+		slog.Error("Start", "error", err)
 		os.Exit(1)
 	}
 }
